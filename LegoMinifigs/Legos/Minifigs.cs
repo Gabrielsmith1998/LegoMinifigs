5dc96ad9b24ccc8bbeb362a69970a65be2bda5c7
16156 <16156@DESKTOP-LEPH36T>
2022-01-11 19:58:01 -0600

    Add project files.

---
+++ LegoMinifigs/Legos/Minifigs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoMinifigs.Legos
+{
+    class Minifigs
+    {
+        public string Head { get; set; }
+        public string Hat { get; set; }
+        public string Torso { get; set; }
+        public string Legs { get; set; }
+        public string Accessories { get; set; }
+        public bool OneHundred { get; set; }
+
+        public void SpecialAttack()
+        {
+            Console.WriteLine("Pew Pew!");
+        }
+
+        public void Unlockable()
+        {
+            Console.WriteLine("You have completed this game!");
+        }
+
+        public void DoubleJump()
+        {
+            Console.WriteLine($"Since you have {Legs} you can double jump!");
+        }
+
+        public void LightsaberAttack()
+        {
+            Console.WriteLine("You can use the lightsaber attack!");
+        }
+    }
+}
---
+++ LegoMinifigs/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using LegoMinifigs.Legos;
+
+namespace LegoMinifigs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minifig1 = new Minifigs();
+            minifig1.Head = "Han Solo Head";
+            minifig1.Hat = "Storm Trooper Hat";
+            minifig1.Legs = "Yoda Legs";
+            minifig1.Torso = "Harry Potter Torso";
+            minifig1.Accessories = "Indiana Jones Revolver";
+            minifig1.OneHundred = true;
+
+            if (minifig1.Accessories == "Indiana Jones Revolver")
+            {
+                minifig1.SpecialAttack();
+            } else
+            {
+                Console.WriteLine("All Outta Bullets!");
+            }
+
+            if (minifig1.OneHundred == true)
+            {
+                minifig1.Unlockable();
+            }
+
+            if (minifig1.Legs == "Yoda Legs")
+            {
+                minifig1.DoubleJump();
+            }
+
+            var minifig2 = new Minifigs();
+            minifig2.Head = "Luke Skywalker Head";
+            minifig2.Hat = "Darth Vader Helmet";
+            minifig2.Legs = "Batman Legs";
+            minifig2.Torso = "Batman Torso";
+            minifig2.Accessories = "Blue Lightsaber";
+            minifig2.OneHundred = false;
+
+            if (minifig2.OneHundred == true)
+            {
+                minifig2.Unlockable();
+            } else
+            {
+                Console.WriteLine("You still have more levels to complete.");
+            }
+
+            if (minifig2.Accessories == "Blue Lightsaber")
+            {
+                minifig2.LightsaberAttack();
+            }
+        }
+    }
+}
